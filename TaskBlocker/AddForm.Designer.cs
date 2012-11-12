31aeec3942ebb211964d8b6240cc344a7b323b85
bacca87 <bacca87@1ea87f4a-031f-4be3-b04c-c47dda1be9f4>
2012-11-12 15:34:53 +0000

    Initial commit!

---
+++ TaskBlocker/AddForm.Designer.cs
@@ -0,0 +1,271 @@
+﻿//
+//  This file is part of Task Blocker, 
+//  an administration tool which can prevent running any application in Windows
+//
+//  Author: Copyright (C) 2010  Marco "SNaKE" Baccarani, <bacca87@gmail.com>
+//
+//  Task Blocker is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by 
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Task Blocker is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Task Blocker.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+namespace TaskBlocker
+{
+    partial class AddForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.taskDataGrid = new System.Windows.Forms.DataGridView();
+            this.nameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fileRadioButton = new System.Windows.Forms.RadioButton();
+            this.manualRadioButton = new System.Windows.Forms.RadioButton();
+            this.processListRadioButton = new System.Windows.Forms.RadioButton();
+            this.fileTextBox = new System.Windows.Forms.TextBox();
+            this.manualTextBox = new System.Windows.Forms.TextBox();
+            this.selectFileButton = new System.Windows.Forms.Button();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.processLabel = new System.Windows.Forms.Label();
+            this.processCountLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.taskDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // taskDataGrid
+            // 
+            this.taskDataGrid.AllowUserToAddRows = false;
+            this.taskDataGrid.AllowUserToDeleteRows = false;
+            this.taskDataGrid.AllowUserToResizeRows = false;
+            this.taskDataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.taskDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.taskDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameColumn,
+            this.idColumn,
+            this.descriptionColumn});
+            this.taskDataGrid.Location = new System.Drawing.Point(31, 183);
+            this.taskDataGrid.MultiSelect = false;
+            this.taskDataGrid.Name = "taskDataGrid";
+            this.taskDataGrid.ReadOnly = true;
+            this.taskDataGrid.RowHeadersVisible = false;
+            this.taskDataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.taskDataGrid.Size = new System.Drawing.Size(698, 252);
+            this.taskDataGrid.TabIndex = 1;
+            // 
+            // nameColumn
+            // 
+            this.nameColumn.HeaderText = "Name";
+            this.nameColumn.Name = "nameColumn";
+            this.nameColumn.ReadOnly = true;
+            this.nameColumn.Width = 250;
+            // 
+            // idColumn
+            // 
+            this.idColumn.HeaderText = "Id";
+            this.idColumn.Name = "idColumn";
+            this.idColumn.ReadOnly = true;
+            this.idColumn.Width = 80;
+            // 
+            // descriptionColumn
+            // 
+            this.descriptionColumn.HeaderText = "Description";
+            this.descriptionColumn.Name = "descriptionColumn";
+            this.descriptionColumn.ReadOnly = true;
+            this.descriptionColumn.Width = 500;
+            // 
+            // fileRadioButton
+            // 
+            this.fileRadioButton.AutoSize = true;
+            this.fileRadioButton.Location = new System.Drawing.Point(12, 71);
+            this.fileRadioButton.Name = "fileRadioButton";
+            this.fileRadioButton.Size = new System.Drawing.Size(67, 17);
+            this.fileRadioButton.TabIndex = 2;
+            this.fileRadioButton.Text = "From File";
+            this.fileRadioButton.UseVisualStyleBackColor = true;
+            this.fileRadioButton.CheckedChanged += new System.EventHandler(this.fileRadioButton_CheckedChanged);
+            // 
+            // manualRadioButton
+            // 
+            this.manualRadioButton.AutoSize = true;
+            this.manualRadioButton.Location = new System.Drawing.Point(12, 14);
+            this.manualRadioButton.Name = "manualRadioButton";
+            this.manualRadioButton.Size = new System.Drawing.Size(95, 17);
+            this.manualRadioButton.TabIndex = 3;
+            this.manualRadioButton.Text = "Enter Manually";
+            this.manualRadioButton.UseVisualStyleBackColor = true;
+            this.manualRadioButton.CheckedChanged += new System.EventHandler(this.manualRadioButton_CheckedChanged);
+            // 
+            // processListRadioButton
+            // 
+            this.processListRadioButton.AutoSize = true;
+            this.processListRadioButton.Location = new System.Drawing.Point(12, 131);
+            this.processListRadioButton.Name = "processListRadioButton";
+            this.processListRadioButton.Size = new System.Drawing.Size(122, 17);
+            this.processListRadioButton.TabIndex = 4;
+            this.processListRadioButton.Text = "From Active Process";
+            this.processListRadioButton.UseVisualStyleBackColor = true;
+            this.processListRadioButton.CheckedChanged += new System.EventHandler(this.processListRadioButton_CheckedChanged);
+            // 
+            // fileTextBox
+            // 
+            this.fileTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.fileTextBox.Location = new System.Drawing.Point(31, 95);
+            this.fileTextBox.Name = "fileTextBox";
+            this.fileTextBox.ReadOnly = true;
+            this.fileTextBox.Size = new System.Drawing.Size(611, 20);
+            this.fileTextBox.TabIndex = 5;
+            // 
+            // manualTextBox
+            // 
+            this.manualTextBox.Location = new System.Drawing.Point(31, 37);
+            this.manualTextBox.Name = "manualTextBox";
+            this.manualTextBox.Size = new System.Drawing.Size(698, 20);
+            this.manualTextBox.TabIndex = 0;
+            // 
+            // selectFileButton
+            // 
+            this.selectFileButton.Location = new System.Drawing.Point(654, 93);
+            this.selectFileButton.Name = "selectFileButton";
+            this.selectFileButton.Size = new System.Drawing.Size(75, 23);
+            this.selectFileButton.TabIndex = 1;
+            this.selectFileButton.Text = "...";
+            this.selectFileButton.UseVisualStyleBackColor = true;
+            this.selectFileButton.Click += new System.EventHandler(this.selectFileButton_Click);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(31, 154);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(75, 23);
+            this.refreshButton.TabIndex = 2;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(654, 445);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "&Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(567, 445);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 4;
+            this.cancelButton.Text = "&Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "exe";
+            this.openFileDialog.Filter = "exe files (*.exe)|*.exe";
+            // 
+            // processLabel
+            // 
+            this.processLabel.AutoSize = true;
+            this.processLabel.Location = new System.Drawing.Point(112, 159);
+            this.processLabel.Name = "processLabel";
+            this.processLabel.Size = new System.Drawing.Size(48, 13);
+            this.processLabel.TabIndex = 6;
+            this.processLabel.Text = "Process:";
+            // 
+            // processCountLabel
+            // 
+            this.processCountLabel.AutoSize = true;
+            this.processCountLabel.Location = new System.Drawing.Point(157, 159);
+            this.processCountLabel.Name = "processCountLabel";
+            this.processCountLabel.Size = new System.Drawing.Size(13, 13);
+            this.processCountLabel.TabIndex = 7;
+            this.processCountLabel.Text = "0";
+            // 
+            // AddForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 480);
+            this.Controls.Add(this.processCountLabel);
+            this.Controls.Add(this.processLabel);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.selectFileButton);
+            this.Controls.Add(this.manualTextBox);
+            this.Controls.Add(this.fileTextBox);
+            this.Controls.Add(this.processListRadioButton);
+            this.Controls.Add(this.manualRadioButton);
+            this.Controls.Add(this.fileRadioButton);
+            this.Controls.Add(this.taskDataGrid);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AddForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Add Task";
+            ((System.ComponentModel.ISupportInitialize)(this.taskDataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView taskDataGrid;
+        private System.Windows.Forms.RadioButton fileRadioButton;
+        private System.Windows.Forms.RadioButton manualRadioButton;
+        private System.Windows.Forms.RadioButton processListRadioButton;
+        private System.Windows.Forms.TextBox fileTextBox;
+        private System.Windows.Forms.TextBox manualTextBox;
+        private System.Windows.Forms.Button selectFileButton;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionColumn;
+        private System.Windows.Forms.Label processLabel;
+        private System.Windows.Forms.Label processCountLabel;
+    }
+}
---
+++ TaskBlocker/IniFile.cs
@@ -0,0 +1,61 @@
+﻿//
+//  This file is part of Task Blocker, 
+//  an administration tool which can prevent running any application in Windows
+//
+//  Author: Copyright (C) 2010  Marco "SNaKE" Baccarani, <bacca87@gmail.com>
+//
+//  Task Blocker is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by 
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Task Blocker is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Task Blocker.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.IO;
+
+namespace TaskBlocker.Tools
+{
+    public class IniFile
+    {
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+        private string m_path;
+
+        public IniFile(string iniPath)
+        {
+            m_path = iniPath;
+        }
+
+        public void writeValue(string section, string key, string value)
+        {
+            WritePrivateProfileString(section, key, value, m_path);
+        }
+
+        public string readValue(string section, string key)
+        {
+            return readValue(section, key, string.Empty);
+        }
+
+        public string readValue(string section, string key, string defaultValue)
+        {
+            StringBuilder temp = new StringBuilder(255);
+                        
+            GetPrivateProfileString(section, key, defaultValue, temp, 255, m_path);                
+            
+            return temp.ToString();
+        }        
+    }
+}
